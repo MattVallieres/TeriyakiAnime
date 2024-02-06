cc51c3ab22ebda9ae00489fe8cb384ce76644c42
Matthew Vallieres <mvallieres@hotmail.ca>
2024-02-06 13:44:52 -0800

    Hero

---
+++ client/app/components/Hero.js
@@ -0,0 +1,14 @@
+export const Hero = () => {
+  return (
+    <div
+      className="relative bg-cover bg-center flex justify-center items-center"
+      style={{ height: "500px", backgroundImage: "url('hero.jpg')", pointerEvents: 'none' }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="text-white relative z-10">
+        <h1 className="text-5xl font-bold mb-2 uppercase text-orange-600 ml-4">Teriyaki Anime</h1>
+        <p className="text-xl ml-4 font-bold uppercase">Experience your favourite anime anytime, anywhere!</p>
+      </div>
+    </div>
+  );
+};
--- client/app/page.js
+++ client/app/page.js
@@ -1,8 +1,9 @@
+import { Hero } from "./components/Hero";
+
 export default function Home() {
   return (
     <>
-    <h1>Hello World!</h1>
-    <a href="/api/auth/login">Login</a>
+    <Hero />
     </>
   );
 }
